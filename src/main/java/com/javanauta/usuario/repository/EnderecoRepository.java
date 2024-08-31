30a7cc96c4cea932e5f59e5ef64b855e1a36e446
Maksuedson <maksuedson@hotmail.com>
2024-08-31 18:28:36 -0300

    Criado repository

--- src/main/java/com/javanauta/usuario/repository/EnderecoRepository.java
+++ src/main/java/com/javanauta/usuario/repository/EnderecoRepository.java
@@ -5,4 +5,6 @@ import com.javanauta.usuario.entity.UsuarioEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface EnderecoRepository extends MongoRepository<EnderecoEntity, String> {
+    EnderecoEntity findByUsuarioId(String usuarioId);
+    void deleteByUsuarioId(String usuarioId);
 }
