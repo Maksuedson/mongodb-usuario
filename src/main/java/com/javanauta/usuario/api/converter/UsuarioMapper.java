8bf91f8c6647e71d241f88eb1321f6e04dc5fa08
Maksuedson <maksuedson@hotmail.com>
2024-08-31 18:28:25 -0300

    Criado mapper e converter

---
+++ src/main/java/com/javanauta/usuario/api/converter/UsuarioMapper.java
@@ -0,0 +1,17 @@
+package com.javanauta.usuario.api.converter;
+
+import com.javanauta.usuario.api.response.UsuarioResponseDTO;
+import com.javanauta.usuario.entity.EnderecoEntity;
+import com.javanauta.usuario.entity.UsuarioEntity;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+@Mapper(componentModel = "spring")
+public interface UsuarioMapper {
+
+    @Mapping(target = "id", source = "usuario.id")
+    @Mapping(target = "nome", source = "usuario.nome")
+    @Mapping(target = "documento", source = "usuario.documento")
+    @Mapping(target = "endereco", source = "enderecoEntity")
+    UsuarioResponseDTO paraUsuarioResponseDTO(UsuarioEntity usuario, EnderecoEntity enderecoEntity);
+
+}
