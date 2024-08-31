25092ee659d119c68befff6f2e8e2bddd69b0375
Maksuedson <maksuedson@hotmail.com>
2024-08-31 18:29:57 -0300

    Adicionado metodo deleteByEmail

--- src/main/java/com/javanauta/usuario/repository/UsuarioRepository.java
+++ src/main/java/com/javanauta/usuario/repository/UsuarioRepository.java
@@ -4,4 +4,6 @@ import com.javanauta.usuario.entity.UsuarioEntity;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UsuarioRepository extends MongoRepository<UsuarioEntity, String> {
+    UsuarioEntity findByEmail(String email);
+    void deleteByEmail(String email);
 }
